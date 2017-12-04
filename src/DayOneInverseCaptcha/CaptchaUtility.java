885a439a7307968708607a9c8358777a27f20fa4
SarahGG <guarino.sgg@gmail.com>
2017-12-04 16:24:07 -0500

    adding CaptchaUtility and it's test class

---
+++ src/DayOneInverseCaptcha/CaptchaUtility.java
@@ -0,0 +1,14 @@
+package DayOneInverseCaptcha;
+
+import java.util.ArrayList;
+
+/**
+ * Performs captcha operations on a captcha string.
+ *
+ * @author Sarah Guarino
+ * @version 1.0
+ */
+
+public class CaptchaUtility {
+
+}
---
+++ test/DayOneInverseCaptcha/CaptchaUtilityTest.java
@@ -0,0 +1,19 @@
+package DayOneInverseCaptcha;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Testing class for the Captcha Utility Class.
+ *
+ * @author Sarah Guarino
+ * @version 1.0
+ */
+
+public class CaptchaUtilityTest {
+
+}
