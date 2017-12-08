b6ac784e6f40db7b51a795fbbeacaa94a3545889
SarahGG <guarino.sgg@gmail.com>
2017-12-08 12:15:20 -0500

    Adding checksumUtility class and corresponding test file

---
+++ src/DayTwoCorruptionCheckSum/ChecksumUtility.java
@@ -0,0 +1,12 @@
+package DayTwoCorruptionCheckSum;
+
+/**
+ * Utility class Advent of Code
+ * Day Two: Corruption Checksum
+ *
+ * @author Sarah Guarino
+ * @version 1.0
+ */
+public class ChecksumUtility {
+
+}
---
+++ test/DayTwoCorruptionCheckSum/ChecksumUtilityTest.java
@@ -0,0 +1,11 @@
+package DayTwoCorruptionCheckSum;
+
+/**
+ * Test class for the ChecksumUtility Class
+ *
+ * @author Sarah Guarino
+ * @version 1.0
+ */
+public class ChecksumUtilityTest {
+
+}
