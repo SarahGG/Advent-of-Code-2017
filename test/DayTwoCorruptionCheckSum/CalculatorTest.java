f16cbb444c23920c7aea2225ee7e3127a6cc5d98
SarahGG <guarino.sgg@gmail.com>
2018-01-07 18:59:30 -0500

    adding getHighestNumber method to the Calculator Class
    accepts an ArrayList of Strings, looks at the numbers in that String and
    return the highest number as an Integer.

--- test/DayTwoCorruptionCheckSum/CalculatorTest.java
+++ test/DayTwoCorruptionCheckSum/CalculatorTest.java
@@ -1,5 +1,12 @@
 package DayTwoCorruptionCheckSum;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Test class for the ChecksumCalculator Class
  *
@@ -7,5 +14,25 @@ package DayTwoCorruptionCheckSum;
  * @version 1.0
  */
 public class CalculatorTest {
+    private String spreadsheetString;
 
+    @Before
+    public void setUp() {
+        spreadsheetString = "5 1 9 5\n" +
+                "7 5 3\n" +
+                "2 4 6 8";
+    }
+
+    @Test
+    public void highestNumbersFromEachRowAreNineSevenAndEight() {
+        SpreadsheetMaker spreadsheetMaker = new SpreadsheetMaker();
+        ArrayList<ArrayList<String>> spreadsheet = spreadsheetMaker.buildSpreadsheet(spreadsheetString);
+        ArrayList<Integer> highestNumbers = new ArrayList<>();
+
+        for (ArrayList<String> rows : spreadsheet) {
+            highestNumbers.add(Calculator.getHighestNumber(rows));
+        }
+
+        assertEquals("[9, 7, 8]", highestNumbers.toString());
+    }
 }
